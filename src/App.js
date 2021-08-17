858acc96fc2d98519542acd5cde472e2fcd5f995
kobimodel5 <sromasterw@gmail.com>
2021-08-17 17:36:35 +0300

    creating a sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="app_body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
