e870119c29421403f99129a611c39f560b082521
kobimodel5 <sromasterw@gmail.com>
2021-08-18 00:01:42 +0300

    some changes in chat.js

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import "./Chat.css";
 import { AttachFile, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Chat() {
@@ -34,9 +36,22 @@ function Chat() {
         </div>
       </div>
 
-      <div className="chat_body"></div>
+      <div className="chat_body">
+        <p className={`chat_message ${true && "chat_receiver"}`}>
+          <span className="chat_name">Dovydas</span>Hey Guys
+          <span className="chat_timestamp">3:52pm</span>
+        </p>
+        <p className="chat_message">Hey Guys</p>
+      </div>
 
-      <div className="chat_footer"></div>
+      <div className="chat_footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" />
+          <button>Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
