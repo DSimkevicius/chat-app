cceca11ef9990f409298c4d0b94f3aec1fb3573a
kobimodel5 <sromasterw@gmail.com>
2021-08-17 15:53:01 +0300

    clean-up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBxnU7ZrGdJLs4081CMOq91ZTN36QCIFqI",
+  authDomain: "chat-app-7c8b1.firebaseapp.com",
+  projectId: "chat-app-7c8b1",
+  storageBucket: "chat-app-7c8b1.appspot.com",
+  messagingSenderId: "603417287555",
+  appId: "1:603417287555:web:c7dd93dd72354e4b6fb4b6",
+  measurementId: "G-T3ZPND6ZF5",
+};
